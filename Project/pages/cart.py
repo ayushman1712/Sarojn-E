cdfc6141009e92f61e83d5eb754f3e25c877fc84
Vijval9 <114911861+Vijval9@users.noreply.github.com>
2024-03-31 14:13:39 +0530

    Update cart.py

--- Project/pages/cart.py
+++ Project/pages/cart.py
@@ -29,19 +29,23 @@ cr.execute("use mydb")
 
 
 def view_cart():
-  if len(st.session_state.cart) > 0:
-    
-    st.subheader("Your Cart")
-    for product_id, quantity in st.session_state.cart.items():
-      #st.write(product_id)
-      # Fetch product details based on product_id
-      cr.execute(f"select Name, Cost from product where ProductID = '{product_id}';")
-      product_data = cr.fetchone()
-      # Display product details and quantity
-      #st.write(f"{product_data[0]} - ${product_data[1]:.2f} (x{quantity})")
-      
-      st.write(f"{product_data[0]} - {product_data[1]*quantity},  ({quantity} items)")
-      # Add buttons for update quantity or remove from cart (optional)
+    if len(st.session_state.cart) > 0:
+        
+        st.subheader("Your Cart")
+        for product_id, quantity in st.session_state.cart.items():
+        #st.write(product_id)
+        # Fetch product details based on product_id
+            cr.execute(f"select Description, Cost from product where ProductID = '{product_id}';")
+            product_data = cr.fetchone()
+            # Display product details and quantity
+            #st.write(f"{product_data[0]} - ${product_data[1]:.2f} (x{quantity})")
+            
+            st.write(f"{product_data[0]} - {product_data[1]*quantity},  ({quantity} items)")
+        
+        
+        # Add buttons for update quantity or remove from cart (optional)
+    if st.button("BACK"):
+        st.switch_page(r"pages\prods.py")
 
 # Call view_cart() after the product listing section
 view_cart()
