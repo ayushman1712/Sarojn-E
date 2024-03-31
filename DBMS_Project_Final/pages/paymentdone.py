0b037ad61f4373cae2184fe1736681eca1638e49
Vijval9 <114911861+Vijval9@users.noreply.github.com>
2024-03-31 23:25:46 +0530

    final

---
+++ DBMS_Project_Final/pages/paymentdone.py
@@ -0,0 +1,41 @@
+from time import sleep
+import mysql.connector
+import streamlit as st
+#from streamlit.state.session_state import SessionState
+
+db = mysql.connector.connect(
+    
+    host = "localhost",
+    user = "root",
+    password = "Duppu0903)()#",
+    database = "mydb"
+    
+    
+)
+
+st.set_page_config(initial_sidebar_state="collapsed")
+st.markdown("""
+    <style>
+    [data-testid="collapsedControl"] { display: none }
+    </style>
+""", unsafe_allow_html=True)
+
+
+
+
+global cr
+cr = db.cursor()
+cr.execute("use mydb")
+
+def paid(cr):
+    
+    st.write("Payment completed!")
+    
+    if(st.button("BAck to Categories")):
+        st.switch_page(r"pages\cat.py")
+        
+paid(cr)      
+        
+    
+    
+    
